0fe84b167557e3e330666be7e12e220bcaacc802
csantosm <4a.santos@gmail.com>
2020-05-12 11:18:29 +0200

    Generated styles

--- custom_theme/js/anchor-links.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    var pathname = window.location.href.split('#')[0];
-    $('a[href^="#"]').each(function () {
-        var $this = $(this),
-            link = $this.attr('href');
-        $this.attr('href', pathname + link);
-    });
-});
