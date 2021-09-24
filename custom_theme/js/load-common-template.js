fbe4dbbb7cfd7e57cfd34aa29f079c7653c8343c
Juan Navarro <juan.navarro@gmx.es>
2021-09-24 12:20:49 +0200

    Fix CORS error loading the Version selector
    `mkdocs serve` (see by opening http://127.0.0.1:8000/) doesn't show the version selector or the version warning:
    
        Access to XMLHttpRequest at 'https://docs.openvidu.io/en/stable/common/version-button.html' from origin 'http://127.0.0.1:8000' has been blocked by CORS policy
    
        Access to XMLHttpRequest at 'https://docs.openvidu.io/en/stable/common/version-warning.html' from origin 'http://127.0.0.1:8000' has been blocked by CORS policy
    
    The problem is that `version-button.html` and `version-warning.html` are loaded by [load-common-template.js](https://github.com/OpenVidu/openvidu.io-docs/blob/2b52da325b456d905da09df45f6afe7380f0db1d/custom_theme/js/load-common-template.js#L7) in this way:
    
        $.ajax({url: "https://docs.openvidu.io/en/stable/common/" + pathToFile, [...]
    
    There's really no need to force the request to `https://docs.openvidu.io/en/stable/`.
    
    Referring to an absolute path (which itself is always relative to the root of the site) solves the issue:
    
        $.ajax({url: "/common/" + pathToFile, [...]
    
    This has the extra effect of making the link portable, now it doesn't matter if the root dir changes in the future.

--- custom_theme/js/load-common-template.js
+++ custom_theme/js/load-common-template.js
@@ -5,15 +5,14 @@ var ELEMENT_ID_TO_REMOVE = document.currentScript.getAttribute('data-elementIdTo
 
 function runAjax(pathToFile, elementId, runAnchorScript, elementIdToRemove) {
     $.ajax({
-        url: "https://docs.openvidu.io/en/stable/common/" + pathToFile,
-        // url: "http://127.0.0.1:8000/common/" + pathToFile,
+        url: "/common/" + pathToFile,
         context: document.body,
         dataType: "html",
         success: response => {
             $('#' + elementId).html(response);
             if (runAnchorScript) {
                 // Transform anchors links to not reload page on click
-                $.getScript('js/anchor-links.js');
+                $.getScript('/js/anchor-links.js');
                 // If the URL contains an anchor, now is the moment to navigate to it
                 if (window.location.hash) {
                     setTimeout(() => $(document.body).scrollTop($(window.location.hash).offset().top), 1);
