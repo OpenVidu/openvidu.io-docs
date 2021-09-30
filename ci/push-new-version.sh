185adf177011ff320e167e6e2e9e812625e7e6e5
Juan Navarro <juan.navarro@gmx.es>
2021-09-30 17:30:13 +0200

    theme: set <base> to the correct path from RTD
    <base> path should not be hardcoded during the release, because the
    exact same code can be built into different paths by Read The Docs.
    
    For example, the last release 2.20.0 is built under "/en/2.20.0/", but
    it also gets built under "/en/stable/".
    
    It is possible to query RTD about the correct value for the path, so do
    that in order to provide a correct <base> and working URLs in all docs.

--- ci/push-new-version.sh
+++ ci/push-new-version.sh
@@ -10,10 +10,4 @@ echo "Building docs for version ${VERSION}" 1>&2
 git clone git@github.com:OpenVidu/openvidu.io-docs.git
 cd openvidu.io-docs
 git branch $VERSION
-git checkout $VERSION
-# Overwrite base href from "/" to "/en/VERSION/"
-sed -i -e 's|<base href="/">|<base href="/en/'${VERSION}'/">|g' custom_theme/base.html
-git add custom_theme/base.html
-git commit -m "Updated base href element"
-git checkout master
 git push origin $VERSION
--- custom_theme/js/load-common-template.js
+++ custom_theme/js/load-common-template.js
@@ -5,14 +5,14 @@ var ELEMENT_ID_TO_REMOVE = document.currentScript.getAttribute('data-elementIdTo
 
 function runAjax(pathToFile, elementId, runAnchorScript, elementIdToRemove) {
     $.ajax({
-        url: "/common/" + pathToFile,
+        url: "common/" + pathToFile,
         context: document.body,
         dataType: "html",
         success: response => {
             $('#' + elementId).html(response);
             if (runAnchorScript) {
                 // Transform anchors links to not reload page on click
-                $.getScript('/js/anchor-links.js');
+                $.getScript('js/anchor-links.js');
                 // If the URL contains an anchor, now is the moment to navigate to it
                 if (window.location.hash) {
                     setTimeout(() => $(document.body).scrollTop($(window.location.hash).offset().top), 1);
